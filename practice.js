3984d36a30ee3242dc7c74d6dc9b75bf1e0b219e
971126chr <971126che@gmail.com>
2021-11-12 12:01:28 +0900

    practice of variable #2

--- practice.js
+++ practice.js
@@ -1,5 +1,12 @@
 const a = 5;
 const b = 2;
+let myName = "HR";
+
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Hye Ryeong";
+
+console.log("Your new name is " + myName);

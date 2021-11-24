34ea20e84b3a7aa21834c04fd28b02d13ffb92bf
971126chr <971126che@gmail.com>
2021-11-24 15:18:09 +0900

    finish remove button click event #24

--- js/todo.js
+++ js/todo.js
@@ -10,11 +10,28 @@ function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+function realDel(deleteToDo) {
+    if(confirm("정말 삭제하시겠습니까?")) {
+        return;
+    }
+    else {
+        //deleteToDo();
+        li.remove();
+    }
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
+    realDel();
     li.remove();
     toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
     saveToDos();
+}
+
+function modifyToDo(paintToDo) {
+    const span = document.querySelector("li span");
+    span.remove();
+    modifyInput.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function paintToDo(newToDo) {
@@ -22,11 +39,17 @@ function paintToDo(newToDo) {
     li.id = newToDo.id;
     const span = document.createElement("span");
     span.innerText = newToDo.text;
-    const button = document.createElement("button");
+    let modifyToDoButton = document.createElement("button");
+    modifyToDoButton.className = "modify-todobtn";
+    modifyToDoButton.innerText = "수정";
+    modifyToDoButton.addEventListener("click", modifyToDo);
+    let button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
+    const modifyInput = document.createElement("input");
+    modifyInput.setAttribute("type", "text");
+    modifyInput.classList.add(HIDDEN_CLASSNAME);
+    li.append(modifyInput, span, modifyToDoButton, button);
     toDoList.appendChild(li);
 }
 
@@ -38,6 +61,7 @@ function handleToDoSubmit(event) {
         text: newToDo,
         id: Date.now()
     }
+    console.log();
     toDos.push(newToDoObj);
     paintToDo(newToDoObj);
     saveToDos();
